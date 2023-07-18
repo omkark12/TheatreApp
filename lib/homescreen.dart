01350eb30c6bd98b20289420514bb3339c9d30e6
omk12 <omkarkatkar2004@gmail.com>
2023-07-18 23:22:47 +0530

    homescreen

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
 
 class homescreen extends StatelessWidget {
-  const homescreen({Key? key}) : super(key: key);
+  List movies = [
+    'Farzand'
+        'Pawankhind'
+        'Kakan'
+        'Shershivraj'
+        'RRR'
+  ];
 
+  List movies2 = [
+    'Farzand'
+        'Pawankhind'
+        'Kakan'
+        'Shershivraj'
+        'RRR'
+  ]
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          Icons.sort,
+          size: 32,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          'OMKARA THEATRE',
+          style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+              fontSize: 26,
+              fontStyle: FontStyle.italic),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'STREAMING NOW',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'VIEW ALL',
+                      style: TextStyle(
+                        color: Colors.orange.shade400,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 390,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          InkWell(
+                            onTap: () {},
+                            child: ClipRect(
+                              child: Image.asset(
+                                'img/${movies[index]}.jpg',
+                                height: 280,
+                              ),
+                            ),
+                          )
+                        ],
+                      ));
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        scaffoldBackgroundColor: Color(0xFF212429),
       ),
       home: welcomescreen(),
     );
