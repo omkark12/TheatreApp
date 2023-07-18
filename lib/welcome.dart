cb4e088351cc419e585f76fece236b5d98f6efda
omk12 <omkarkatkar2004@gmail.com>
2023-07-18 21:20:16 +0530

    welcomepage

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:theatreapp/homescreen.dart';
 
 class welcomescreen extends StatelessWidget {
   const welcomescreen({Key? key}) : super(key: key);
@@ -10,9 +11,58 @@ class welcomescreen extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.black,
           image: DecorationImage(
-              image: AssetImage('img/back1.jpg'),
+              image: AssetImage('img/back.jpg'),
               fit: BoxFit.cover,
-              opacity: 0.4),
+              opacity: 0.5),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "OMKARA THEATRE",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontStyle: FontStyle.italic),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Get your Ticket now",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => homescreen()));
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                    color: Colors.orange.shade400,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  "Less go",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
